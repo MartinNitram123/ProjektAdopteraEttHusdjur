a08d41517d0f0f506c8950ea8ee3892f03b94d56
AlexPettersson <115615087+AlexPettersson@users.noreply.github.com>
2024-01-24 09:30:25 +0000

    Initial commit

---
+++ helper.py
@@ -0,0 +1,49 @@
+pets = {
+    'dogs': [{
+        'name':
+        'Spot',
+        'age':
+        2,
+        'breed':
+        'Dalmatian',
+        'description':
+        'Spot is an energetic puppy who seeks fun and adventure!',
+        'url':
+        'https://content.codecademy.com/programs/flask/introduction-to-flask/dog-spot.jpeg'
+    }, {
+        'name':
+        'Shadow',
+        'age':
+        4,
+        'breed':
+        'Border Collie',
+        'description':
+        'Eager and curious, Shadow enjoys company and can always be found tagging along at your heels!',
+        'url':
+        'https://content.codecademy.com/programs/flask/introduction-to-flask/dog-shadow.jpeg'
+    }],
+    'cats': [{
+        'name':
+        'Snowflake',
+        'age':
+        1,
+        'breed':
+        'Tabby',
+        'description':
+        'Snowflake is a playful kitten who loves roaming the house and exploring.',
+        'url':
+        'https://content.codecademy.com/programs/flask/introduction-to-flask/cat-snowflake.jpeg'
+    }],
+    'rabbits': [{
+        'name':
+        'Easter',
+        'age':
+        83,
+        'breed':
+        'Large one',
+        'description':
+        'Easter is a sweet, gentle rabbit who likes spending most of the day sleeping. when they hit the neigh',
+        'url':
+        'https://i.dailymail.co.uk/1s/2021/01/26/17/38504554-0-image-m-11_1611680480321.jpgrabbiteaster'
+    }]
+}
